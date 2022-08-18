6bc1296b9dc2e5cd2300d332ec31da3fc0c1a4b2
shigang-zhang <71486686+shigang-zhang@users.noreply.github.com>
2022-08-18 19:43:04 +0800

    2

--- src/main/java/com/itheima/web/AccountController.java
+++ src/main/java/com/itheima/web/AccountController.java
@@ -22,7 +22,7 @@ public class AccountController {
     public List<Account> findAccount(){
         System.out.println("张仕刚");
         List<Account> accountList = accountService.findAll();
-        System.out.println("dsfjhgfd");
+        System.out.println("123");
         return accountList;
     }
 }
