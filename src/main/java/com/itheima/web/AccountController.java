b8409008b164f950e23b3f00a8759f2fa04ba49c
张仕刚 <1441705828@qq.com>
2022-08-18 19:54:52 +0800

    往上

--- src/main/java/com/itheima/web/AccountController.java
+++ src/main/java/com/itheima/web/AccountController.java
@@ -22,7 +22,7 @@ public class AccountController {
     public List<Account> findAccount(){
         System.out.println("张仕刚");
         List<Account> accountList = accountService.findAll();
-        System.out.println("123");
+        System.out.println("往上");
         return accountList;
     }
 }
